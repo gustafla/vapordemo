7c10624138b37c57b96b00210d2a0af96c1e7e10
Lauri Gustafsson <luutifa@gmail.com>
2016-01-30 04:10:48 +0200

    Integration

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "window.hpp"
+#include "config.hpp"
+#include "application.hpp"
+#include "vectors.hpp"
+#include <vector>
+#include <cstdlib>
+#include "graphics.hpp"
+
+void cleanup() {
+    #ifdef RASPI_BUILD
+        bcm_host_deinit();
+    #else
+        SDL_Quit();
+    #endif
+}
+
+int main(int argc, char* argv[]) {
+    #ifdef RASPI_BUILD
+        bcm_host_init();
+    #endif
+    const Config conf(argc, argv);
+    Window window(conf);
+    atexit(cleanup);
+    Application testApp(window);
+
+    float frames=0;
+    float tLast=0;
+    const float TIME=5.0f;
+
+    while(testApp.isRunning()) {
+        testApp.draw();
+        
+        frames+=1;
+        if (tLast+TIME < window.getTime()) {
+            std::cout << "FPS: " << (frames/TIME) << std::endl;
+            frames=0;
+            tLast=window.getTime();
+        }
+        
+        check();
+    }
+
+    return 0;
+}
